3ce4bc8d0f52a4293c2ab2c98abaeb0445304efe
Pawanpreet Singh <pawan.iitr08@gmail.com>
2025-07-20 03:38:42 +0530

    Create types.ts

---
+++ src/src/types.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const testimonialSchema = z.object({
+  text: z.string(),
+  author: z.string().optional(),
+});
+
+export const badgeSchema = z.object({
+  title: z.string(),
+  imageUrl: z.string().url(),
+});
+
+export const topmateRecapSchema = z.object({
+  profilePicUrl: z.string().url(),
+  username: z.string(),
+  firstName: z.string(),
+  bookingCount: z.number(),
+  avgRating: z.number(),
+  mostBookedServiceTitle: z.string(),
+  mostBookedServiceType: z.enum(['1:1', 'Digital Product', 'Webinar', 'Package', 'Priority DM']),
+  testimonialCount: z.number(),
+  testimonials: z.array(testimonialSchema),
+  dateOfJoining: z.string(),
+  badges: z.array(badgeSchema),
+  percentileRank: z.number(),
+  userCategory: z.string(),
+});
+
+export type TopmateRecapProps = z.infer<typeof topmateRecapSchema>;
