e7134dc5adf80e8931ca0ee110317e6e89fb7d69
Pawanpreet Singh <pawan.iitr08@gmail.com>
2025-07-20 03:40:51 +0530

    Create video.tsx

---
+++ src/video.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Series } from 'remotion';
+import { z } from 'zod';
+import { topmateRecapSchema } from './types';
+import { IntroScene } from './scenes/IntroScene';
+import { StatsScene } from './scenes/StatsScene';
+import { StarPerformerScene } from './scenes/StarPerformerScene';
+import { EliteStatusScene } from './scenes/EliteStatusScene';
+import { TestimonialScene } from './scenes/TestimonialScene';
+import { TrophyScene } from './scenes/TrophyScene';
+import { OutroScene } from './scenes/OutroScene';
+
+export const Main: React.FC<z.infer<typeof topmateRecapSchema>> = (props) => {
+  return (
+    <Series>
+      <Series.Sequence durationInFrames={90}>
+        <IntroScene profilePicUrl={props.profilePicUrl} firstName={props.firstName} />
+      </Series.Sequence>
+      
+      <Series.Sequence durationInFrames={120}>
+        <StatsScene
+          bookingCount={props.bookingCount}
+          avgRating={props.avgRating}
+          testimonialCount={props.testimonialCount}
+        />
+      </Series.Sequence>
+      
+      <Series.Sequence durationInFrames={150}>
+        <StarPerformerScene
+          mostBookedServiceTitle={props.mostBookedServiceTitle}
+          mostBookedServiceType={props.mostBookedServiceType}
+        />
+      </Series.Sequence>
+      
+      <Series.Sequence durationInFrames={120}>
+        <EliteStatusScene
+          percentileRank={props.percentileRank}
+          userCategory={props.userCategory}
+        />
+      </Series.Sequence>
+      
+      {props.testimonials.length > 0 && (
+        <Series.Sequence durationInFrames={180}>
+          <TestimonialScene testimonials={props.testimonials} />
+        </Series.Sequence>
+      )}
+      
+      {props.badges.length > 0 && (
+        <Series.Sequence durationInFrames={150}>
+          <TrophyScene badges={props.badges} />
+        </Series.Sequence>
+      )}
+      
+      <Series.Sequence durationInFrames={120}>
+        <OutroScene dateOfJoining={props.dateOfJoining} />
+      </Series.Sequence>
+    </Series>
+  );
+};
